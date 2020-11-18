1fee7241daf010e9888f150a1ade0c7bf25bc973
alswjd1125 <alslal1125@naver.com>
2020-11-18 16:57:09 +0900

    fetch
    fetch

---
+++ js/bugerBtn.js
@@ -0,0 +1,7 @@
+        $(function() {
+        $('.bugerBtn').click(function(){
+            $(this).toggleClass("active");
+            $('.bugerBtnBox').toggleClass('big');
+            $('.menu ul').toggleClass('textMove');
+	});
+});
