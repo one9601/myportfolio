9af7e519255a72cd1150758fb73229d4e74f930d
alswjd1125 <alslal1125@naver.com>
2020-11-18 17:01:07 +0900

    delete
    delete

--- js/bugerBtn.js
+++
@@ -1,7 +0,0 @@
-        $(function() {
-        $('.bugerBtn').click(function(){
-            $(this).toggleClass("active");
-            $('.bugerBtnBox').toggleClass('big');
-            $('.menu ul').toggleClass('textMove');
-	});
-});
